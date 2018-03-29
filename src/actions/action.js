bcd6cfbfd3e6401cde3a230932069de504466313
Perry <perrymiranda1@yahoo.com>
2018-03-30 01:09:08 +0900

    More fun testing with React with Redux@

--- src/actions/action.js
+++ src/actions/action.js
@@ -1,1 +1,14 @@
 export const INCREMENT = 'INCREMENT';
+
+/*
+
+Example:
+
+export const addArticle = article => ({ type: "ADD_ARTICLE", payload: article });
+
+
+Source:
+https://www.valentinog.com/blog/react-redux-tutorial-beginners/#React_Redux_tutorial_who_this_guide_is_for
+
+
+*/
--- src/index.js
+++ src/index.js
@@ -17,11 +17,26 @@ import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux'
 import { createStore } from 'redux';
 import reducer from './reducers/reducer';
-
+import { INCREMENT } from "./constants/action-types";
 const defaultState = {
 	count: 0
 }
 const store = createStore(reducer, defaultState);
+
+/*
+* This is used for testing Redux?
+*
+* You can run such things as:
+* getState for accessing the current state of the application
+* dispatch for dispatching an action
+* subscribe for listening on state changes
+*
+* Source: https://www.valentinog.com/blog/react-redux-tutorial-beginners/#React_Redux_tutorial_who_this_guide_is_for
+*/
+window.someStore = store;
+
+export const addArticle = counter => ({ type: INCREMENT});
+window.someAddArticle = addArticle;
 
 // const store = createStore(reducer);
 
