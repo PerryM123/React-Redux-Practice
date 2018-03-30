db0f8a96f73f5507fd7a6224962c94e6b20c4887
PerryM123 <perrymiranda1@yahoo.com>
2018-03-30 16:39:45 +0900

    Having trouble figuring out how to implement connect to a component...

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,32 +1,43 @@
 import React, { Component } from 'react';
 import OddEvenMessage from './../components/OddEvenMessage';
+import { connect } from "react-redux";
+import PropTypes from 'prop-types';
+import {increaseValue} from './../actions/index';
+
+
+// Map Redux state to component props
+function mapStateToProps(state) {
+  return {
+    value: state.count
+  }
+}
+
+// Map Redux actions to component props
+function mapDispatchToProps(dispatch) {
+  return {
+    clickHandler: () => dispatch(increaseValue)
+  }
+}
 
 class Display extends Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			value: 0
-		};
-
-		this.clickHandler = this.clickHandler.bind(this);
-	}
-	clickHandler() {
-		this.setState({
-			value: this.state.value + 1
-		});
-	}
   render() {
+  	const { value, clickHandler } = this.props;
     return (
       <div>
         <div className="valueBox">
-					<div className="value">{this.state.value}</div>
-					<input type="button" value="+" onClick={this.clickHandler} />
+			<div className="value">{this.state.value}</div>
+			<input type="button" value="+" onClick={this.clickHandler} />
         </div>
         <OddEvenMessage value={this.state.value} />
       </div>
     );
   }
 }
+
+Display.propTypes = {
+  value: PropTypes.number.isRequired,
+  clickHandler: PropTypes.func.isRequired
+}
+const App = connect(mapStateToProps, mapDispatchToProps)(Display);
 
 export default Display;
